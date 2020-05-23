4057eb445249ff7fc4fdea2529d8d4b0ee323406
sliao888 <simon.liao19@gmail.com>
2020-05-22 19:17:59 -0700

    add fund data format method

--- attribution.py
+++ attribution.py
@@ -4,22 +4,27 @@ import numpy as np
 
 class Attribution():
     
-    def __init__(self, fundData, marketData):
+    def __init__(self, date, fundDataPath, marketDataPath):
         """[This class contains the methods to calculate performance attribution]
 
         Args:
             fundData ([str]): [path to Fund Data csv]
             marketData ([str]): [path to Market Data csv]
         """
-        self.fundData = fundData
-        self.marketData = marketData
+        self.fundDataPath = fundDataPath
+        self.marketDataPath = marketDataPath
 
-    # def getFundData(self):
+        # bring in the data into DataFrames
+        self.fundData = pd.read_csv(self.fundDataPath)
+        self.marketData = pd.read_csv(self.marketDataPath)
+
+    def formatFundData(self):
+        data = self.fundData.groupby('Sector')
+
         
-
 if __name__ == '__main__':
 
     fundDataPath = r'C:\Users\simon\Documents\Python Scripts\perfAttribution\Data\BrinsonTask_FundData.csv'
     marketDataPath = r'C:\Users\simon\Documents\Python Scripts\perfAttribution\Data\BrinsonTask_MarketData.csv'
 
-    test = Attribution(fundData=fundDataPath, marketData=marketDataPath)
+    test = Attribution(date='2019-31-03', fundDataPath=fundDataPath, marketDataPath=marketDataPath)
